70cf34de0ea9b54437400bc78a30797be3611f29
Stev_Dev <74199611+stev170@users.noreply.github.com>
2022-10-26 06:26:06 +0700

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -29,7 +29,7 @@ TW_MAIN_VERSION=$(sed -n -e 's/^.*#define TW_MAIN_VERSION_STR //p' bootable/reco
 cd out/target/product/${DEVICE}
 
 #Rename build
-mv -v $OUTPUT PBRP-${DEVICE}.img
+mv -v $OUTPUT PBRP-${TW_MAIN_VERSION}-${TW_DEVICE_VERSION}-${DEVICE}.zip
 
 # Upload to oshi.at
 if [ -z "$TIMEOUT" ];then
