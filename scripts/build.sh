4a04881b2da4401415c7e142b62bc6486182dc5d
Stev_Dev <74199611+stev170@users.noreply.github.com>
2022-10-26 15:47:30 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,68 +1,33 @@
 #!/bin/bash
 
-# Source Configs
 source $CONFIG
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
-
 # Change to the Source Directry
-cd $SYNC_PATH
-
-# Set-up ccache
-if [ -z "$CCACHE_SIZE" ]; then
-    ccache -M 10G
-else
-    ccache -M ${CCACHE_SIZE}
-fi
+cd ~/twrp
 
 # Send the Telegram Message
 
 echo -e \
 "
-🛠️ CI|PBRP recovery
-
+✨ PBRP Recovery CI
 ✔️ The Build has been Triggered!
-
-📱 Device: "${DEVICE}"
-🖥 Build System: "${PBRP_BRANCH}"
-🌲 Logs: <a href=\"https://cirrus-ci.com/build/${CIRRUS_BUILD_ID}\">Here</a>
-" > tg.html
+📱 Device: "${DEVICE}
 
 TG_TEXT=$(< tg.html)
 
 telegram_message "${TG_TEXT}"
 echo " "
 
-# Run the Extra Command
-$EXTRA_CMD
-
-# export some Basic Vars
-export ALLOW_MISSING_DEPENDENCIES=true
-
 # Prepare the Build Environment
 source build/envsetup.sh
 
+# export some Basic Vars
+export ALLOW_MISSING_DEPENDENCIES=true
+#export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+#export LC_ALL="C"
+
 # lunch the target
-if [ "$PBRP_BRANCH" = "android-12.1" ]; then
-    lunch omni_${DEVICE}-eng || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-else
-    lunch omni_${DEVICE}-eng || { echo "ERROR: Failed to lunch the target!" && exit 1; }
-fi
-
-# Build the Code
-if [ -z "$J_VAL" ]; then
-    mka -j$(nproc --all) $TARGET || { echo "ERROR: Failed to Build PBRP!" && exit 1; }
-elif [ "$J_VAL"="0" ]; then
-    mka $TARGET || { echo "ERROR: Failed to Build PBRP!" && exit 1; }
-else
-    mka -j${J_VAL} $TARGET || { echo "ERROR: Failed to Build PBRP!" && exit 1; }
-fi
-
+lunch omni_${DEVICE}-eng   || { echo "ERROR: Failed to lunch the target!" && exit 1; }
+mka -j$(nproc --all) pbrp
 # Exit
 exit 0
