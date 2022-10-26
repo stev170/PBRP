f38d1019f88a59d8126566fb431dbf750316f26e
Stev_Dev <74199611+stev170@users.noreply.github.com>
2022-10-27 02:06:59 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,17 +2,21 @@
 
 # Device
 export DT_LINK="https://github.com/PitchBlackRecoveryProject/android_device_xiaomi_ginkgo-pbrp.git -b android-12.1"
-
+export PBRP_MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb"
+export PBRP_BRANCH="android-12.1"
 export DEVICE="ginkgo"
 export OEM="xiaomi"
 export TARGET="recoveryimage"
-
 export OUTPUT="recovery.img"
 
 # Extra Command
 #export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
 
 # Not Recommended to Change
+if [ ! -e $HOME/work ]; then
+mkdir $HOME/work
+fi
+
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
 export USE_CCACHE=1
 export CCACHE_SIZE="50G"
