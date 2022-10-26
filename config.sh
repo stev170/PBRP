23b324c4ddbf91f635d8f20a2b39018b4940002b
Stev_Dev <74199611+stev170@users.noreply.github.com>
2022-10-26 15:53:25 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,40 +1,27 @@
 #!/bin/bash
 
 # Device
-export TWRP_MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb"
-export TWRP_BRANCH="android-12.1"
-#export TWRP_BRANCH="twrp-test"
 export DT_LINK="https://github.com/PitchBlackRecoveryProject/android_device_xiaomi_ginkgo-pbrp.git -b android-12.1"
 
 export DEVICE="ginkgo"
 export OEM="xiaomi"
-#export PLATFORM="msm8998"
-export TARGET="pbrp"
-export TW_DEVICE_VERSION="1"
-export OUTPUT="PBRP*.zip"
-export FILENAME="PBRP*.zip"
+export TARGET="recoveryimage"
 
-# Kernel Source
-# Uncomment the next line if you want to clone a kernel source.
-#export KERNEL_SOURCE="https://github.com/faoliveira78/android_kernel_oneplus_msm8998"
+export OUTPUT="recovery.img"
 
 # Extra Command
-export EXTRA_CMD=""
+#export EXTRA_CMD="git clone https://github.com/OrangeFoxRecovery/Avatar.git misc"
 
 # Not Recommended to Change
-if [ ! -e $HOME/work ]; then
-mkdir $HOME/work
-fi
-
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
 export USE_CCACHE=1
 export CCACHE_SIZE="50G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
 
-if [ ! -z "$PLATFORM" ]; then
-    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
-else
-    export KERNEL_PATH="kernel/$OEM/$DEVICE"
-fi
+#if [ ! -z "$PLATFORM" ]; then
+#    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+#else
+#   export KERNEL_PATH="kernel/$OEM/$DEVICE"
+#fi
 export DT_PATH="device/$OEM/$DEVICE"
